4be2f31b5f9c884d7c1a9a9dad39f3b907c4943a
renard <renard@macbook-pro-de-renard.home>
2019-02-16 17:26:39 +0100

    start boot213

---
+++ src/main/java/com/qc/boot213/Boot213Application.java
@@ -0,0 +1,14 @@
+package com.qc.boot213;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot213Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Boot213Application.class, args);
+	}
+
+}
+
